b443db1b557ee18ed04f48ab22f61f47c399cfa1
Luke Sorensen <lsorensen@wustl.edu>
2016-11-29 22:01:27 -0600

    wave in corners, scaling

--- js/myScript.js
+++ js/myScript.js
@@ -1,65 +1,60 @@
 var width, height, center;
-var points = 1;
+var points = 3;
 var smooth = true;
 var path = new Path({
-    fillColor: 'black'
+    strokeColor: 'pink',
+    strokeWidth: 50
 });
+
 var mousePos = view.center / 2;
 var pathHeight = mousePos.y;
+
+initWidth = view.size.width;
+
 initializePath();
 
-
-function addPoint () {
-    points++;
-    initializePath();   
-}
-
 function initializePath() {
-    center = view.center;
-    width = view.size.width;
-    height = view.size.height / 2;
-    path.segments = [];
-    path.add(view.bounds.bottomLeft);
-    for (var i = 1; i < points; i++) {
-        var point = new Point(width / points * i, center.y);
-        path.add(point);
-    }
-    path.add(view.bounds.topRight);
+	center = view.center;
+	width = view.size.width;
+	height = view.size.height / 2;
+    
+	path.segments = [];
+	path.add(view.bounds.bottomLeft);
+	for (var i = 1; i < points; i++) {
+		var point = new Point(width / points * i, center.y);
+		path.add(point);
+	}
+    
+	path.add(view.bounds.topRight);
     path.add(view.bounds.bottomRight);
-    path.fullySelected = true;
-    path.fillColor('red');
+	path.fullySelected = true;
 }
 
-function onFrame(event) {
+function flex() {
+	
     pathHeight += (center.y - mousePos.y - pathHeight) / 10;
-    for (var i = 1; i < points; i++) {
-        var sinSeed = event.count + (i + i % 10) * 100;
-        var sinHeight = Math.sin(sinSeed / 200) * pathHeight;
-        var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
-        path.segments[i].point.y = yPos;
-    }
-    if (smooth)
-        path.smooth({ type: 'continuous' });
+    h = initWidth-view.size.width;
+    
+	for (var i = 1; i < points; i++) {
+		var sinSeed = h + (i + i % 10) * 100;
+		var sinHeight = Math.sin(sinSeed / 200) * height;
+
+		var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
+		path.segments[i].point.y = yPos;
+
+	}
+	if (smooth)
+		path.smooth({ type: 'continuous' });
 }
 
 function onMouseMove(event) {
-    mousePos = event.point;
+	mousePos = event.point;
 }
 
-function onMouseDown(event) {
-    addPoint();
-    //smooth = !smooth;
-    //if (!smooth) {
-    //    // If smooth has been turned off, we need to reset
-    //    // the handles of the path:
-    //    for (var i = 0, l = path.segments.length; i < l; i++) {
-    //        var segment = path.segments[i];
-    //        segment.handleIn = segment.handleOut = null;
-    //    }
-    //}
-}
 
 // Reposition the path whenever the window is resized:
 function onResize(event) {
-    initializePath();
+    
+	initializePath();
+    flex();
 }
