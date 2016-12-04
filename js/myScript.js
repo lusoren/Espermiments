c8a90bd84187185a7e0efa95a0fb0843d781d486
Luke Sorensen <lsorensen@wustl.edu>
2016-12-04 15:32:51 -0600

    multi lines on click
    CRASHES COMPPPOOOTER SOMETIMES

--- js/myScript.js
+++ js/myScript.js
@@ -1,36 +1,69 @@
 var width, height, center;
 var points = 3;
 var smooth = true;
-var path = new Path({
-    strokeColor: 'pink',
-    strokeWidth: 50
-});
 
 var mousePos = view.center / 2;
 var pathHeight = mousePos.y;
 
 initWidth = view.size.width;
 
-initializePath();
+var paths=[];
+var rand=[];
+pathCounter=0;
 
-function initializePath() {
+
+for (var i=0; i<10;++i){
+    rand[i]=(Math.random()*20)-40;
+}
+
+addPath();
+
+function addPath() {
+
+    var path = new Path({
+        strokeColor: 'pink',
+        strokeWidth: 20,
+        opacity: 0.33
+    });
+    
+    paths[pathCounter]=path;
+    initializePath(paths[pathCounter]);
+    
+    pathCounter++;
+    
+}
+
+
+function initializePath(path) {
 	center = view.center;
 	width = view.size.width;
 	height = view.size.height / 2;
     
 	path.segments = [];
-	path.add(view.bounds.bottomLeft);
+    
+    if (Math.random()*2<1) {
+       path.add(0,height);
+    } else {
+        path.add(view.bounds.bottomLeft)
+    }
+	
+    
 	for (var i = 1; i < points; i++) {
 		var point = new Point(width / points * i, center.y);
 		path.add(point);
 	}
     
-	path.add(view.bounds.topRight);
-    path.add(view.bounds.bottomRight);
+    if (Math.random()*2<1) {
+       path.add(width,height);
+    } else {
+        path.add(view.bounds.bottomRight)
+    }
+	
+    
 	path.fullySelected = true;
 }
 
-function flex() {
+function flex(path) {
 	
     pathHeight += (center.y - mousePos.y - pathHeight) / 10;
     h = initWidth-view.size.width;
@@ -40,7 +73,9 @@ function flex() {
 		var sinHeight = Math.sin(sinSeed / 200) * height;
 
 		var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
-		path.segments[i].point.y = yPos;
+        
+        yPos =( Math.random()*10)-20 +yPos;
+        path.segments[i].point.y = yPos;
 
 	}
 	if (smooth)
@@ -48,13 +83,18 @@ function flex() {
 }
 
 function onMouseMove(event) {
-	mousePos = event.point;
+	addPath();
 }
 
 
 // Reposition the path whenever the window is resized:
 function onResize(event) {
+    for (var i = 0; i < pathCounter; i++) {
+	
+        initializePath(paths[i]);
+        flex(paths[i]);
     
-	initializePath();
-    flex();
+    }
+    
+
 }
