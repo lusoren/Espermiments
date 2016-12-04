835264a6ac8645d3f3a08bb351c4fbf64d43a2a2
Luke Sorensen <lsorensen@wustl.edu>
2016-12-04 17:10:50 -0600

    WOAH

--- js/myScript.js
+++ js/myScript.js
@@ -2,11 +2,18 @@ var width, height, center;
 var points = 3;
 var smooth = true;
 
+var mousePos = view.center / 2;
+var pathHeight = mousePos.y;
+
 var paths=[];
 var pathCounter=0;
-var moveCounter=0;
 
-console.log("ss");
+var moveCounter=0;
+var addTracker=0;
+
+console.log("started");
+addPath();
+
 
 function addPath() {
 
@@ -16,18 +23,25 @@ function addPath() {
     });
     
     curWidth = view.size.width;
+    
+    var ritRand=Math.floor(Math.random()*3);
+    var letRand=Math.floor(Math.random()*3);
+    
     var pathObject = {
         path:path,
-        width:curWidth
+        width:curWidth,
+        right:ritRand,
+        left:letRand
     };
 
     console.log(pathObject.width);
+    console.log(pathObject.left);
+    console.log(pathObject.right);
     
     paths[pathCounter]=pathObject;
     initializePath(paths[pathCounter]);
     
     pathCounter++;
-    
 }
 
 
@@ -41,14 +55,28 @@ function initializePath(pathObject) {
     
 	path.segments = [];
     
-    path.add(view.bounds.bottomLeft);
+    if (pathObject.left==0) {
+        path.add(0,0);
+        
+    } else if (pathObject.left==1) {
+        path.add(0,height);
+        
+    } else {
+        path.add(view.bounds.topLeft);
+    }
     
 	for (var i = 1; i < points; i++) {
 		var point = new Point(width / points * i, center.y);
 		path.add(point);
 	}
     
-    path.add(width,height);
+    if (pathObject.right==0) {
+        path.add(view.bounds.bottomRight);
+    } else if (pathObject.right==1) {
+        path.add(width,height);
+    } else {
+        path.add(view.bounds.topRight);
+    }
 	
 }
 
@@ -58,7 +86,6 @@ function flex(pathObject) {
 	
     pathHeight += (center.y - mousePos.y - pathHeight) / 10;
     h = pathObject.width-view.size.width;
-    
     
 	for (var i = 1; i < points; i++) {
 		var sinSeed = h + (i + i % 10) * 100;
@@ -71,24 +98,27 @@ function flex(pathObject) {
 		path.smooth({ type: 'continuous' });
 }
 
-function onMouseDown(event) {
-	addPath();
-}
+//function onMouseDown(event) {
+//	addPath();
+//}
 
 
 // Reposition the path whenever the window is resized:
 function onResize(event) {
+    moveCounter++
+
+    if ((moveCounter-addTracker)>100) {
+        console.log("added");
+        addTracker=moveCounter;
+        addPath();
+    }
     
-    movCounter++;
-    if (moveCounter%2==0) {
-        for (var i = 0; i < pathCounter; i++) {
+    for (var i = 0; i < pathCounter; i++) {
 	
         initializePath(paths[i]);
         flex(paths[i]);
     
     }
-    }
-    
     
 
 }
